14c57787411f1416b8756e4e6f857bb9281429af
vishnuallada <139074664+vishnuallada@users.noreply.github.com>
2023-10-29 20:07:52 -0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,6 @@
+function addUser() {
+    user_name = document.getElementById("user_name").value;
+    console.log(user_name);
+    localStorage.setItem("username",user_name);
+    window.location = "kwitter_room.html";
+}
